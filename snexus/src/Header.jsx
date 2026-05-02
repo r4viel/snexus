948caa9cc8a00809141a92f78d2c4ce04a649b33
raviel <127047447+r4viel@users.noreply.github.com>
2026-05-02 14:34:55 -0300

    Refactor Header component to use a navigation menu

--- snexus/src/Header.jsx
+++ snexus/src/Header.jsx
@@ -9,15 +9,29 @@ function Header() {
         <div className="logo">
           <img src={logo} alt="Logo" width={120}/>
         </div>
-        <a href="#contact" className="nav-link">Contato</a>
-        <a href="#contact" className="nav-link">Sobre nós</a>
-        <a href="#servicos" className="nav-link">Serviços</a>
+        <nav id="menu" className="menu">
+          <ul>
+            <li><a href="#contact" className="nav-link">Contato</a></li>
+            <li><a href="#contact" className="nav-link">Sobre nós</a></li>
+            <li><a href="#servicos" className="nav-link">Serviços</a></li>
+          </ul>
+        </nav>
         <button className="nav-button" onClick={() => {
           const message = "Olá, gostaria de falar com um especialista sobre os serviços da Snexus.";
           const phoneNumber = "5584999891704";
           const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
           window.open(whatsappUrl, '_blank');
         }}>Fale conosco</button>
+        <button id="menu-btn" onClick={() => {
+          const menu = document.getElementById('menu');
+          const btn = document.getElementById('menu-btn');
+          menu.classList.toggle('active');
+          document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target) && !btn.contains(e.target)) {
+    menu.classList.remove("active");
+  }
+});
+        }}>☰</button>
     </div>
     );
 }
